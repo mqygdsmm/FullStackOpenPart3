ba08dc9153faf3fd917a3ca0f6d3ee23c431e5c5
mqyg <mqygywl@outlook.com>
2024-03-13 16:22:40 +0800

    finished exercise3.7&3.8

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+morgan.token('postData', (req, res) => {return JSON.stringify(req.body)})
+const customFormat = ':method :url :status :res[content-length] - :response-time ms (:postData)';
 
 app.use(express.json())
-
+app.use(morgan(customFormat));
 let persons = [
     { 
       "id": 1,

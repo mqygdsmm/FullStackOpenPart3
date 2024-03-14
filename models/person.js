81488f432ab0052cb90c07189f1cf45c860529e7
mqyg <mqygywl@outlook.com>
2024-03-14 14:25:47 +0800

    finished exercise3.22

--- models/person.js
+++ models/person.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
-console.log(url)
+
+const url = process.env.MONGODB_URI;
+console.log(url);
 
 mongoose.connect(url)
 .then(result => {
